8938ab7c8285174d7c0ab9b81e1df495d3b81e28
atsushi130 <atsushi_main@outlook.jp>
2018-03-18 21:44:30 +0900

    update: eisu and kana

--- keyboards/ergodox_ez/keymaps/atsushi/keymap.c
+++ keyboards/ergodox_ez/keymaps/atsushi/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Hyper|           | ~L1  |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1|  '"  |  Cmd | Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L1  |
+ *   |Grv/L1|  '"  |  Cmd | Left | Eisu |                                       | Kana | Down |   [  |   ]  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(MDIA),
         CTL_T(KC_ESC),  KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        LT(SYMB,KC_GRV),KC_QUOT,      GUI_T(KC_QUOT),  KC_LEFT,KC_RGHT,
+        LT(SYMB,KC_GRV),KC_QUOT,      GUI_T(KC_QUOT),  KC_LEFT,GUI_T(KC_LANG2),
                                               ALT_T(KC_APP),  KC_LGUI,
                                                               KC_HOME,
                                                KC_SPC,KC_BSPC,KC_END,
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              LT(SYMB,KC_GRV), KC_Y, KC_U,  KC_I,    KC_O,    KC_P,              KC_BSPC,
                               KC_H, KC_J,  KC_K,    KC_L,    LT(MDIA, KC_SCLN), KC_ENT,
              LT(SYMB,KC_GRV), KC_N, KC_M,  KC_COMM, KC_DOT,  KC_SLSH,           KC_RSFT,
-                                    KC_UP, KC_DOWN, KC_LBRC, KC_RBRC,           KC_RO,
+                         GUI_T(KC_LANG1),  KC_DOWN, KC_LBRC, KC_RBRC,           KC_RO,
              KC_LALT,        CTL_T(KC_ESC),
              KC_PGUP,
              KC_FN1, KC_BSPC, KC_ENT
